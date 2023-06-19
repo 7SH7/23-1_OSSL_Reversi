233e329c56a519082afa61956926e00f18c61a8d
7SH7 <kimseunghwan7777@gmail.com>
2023-06-19 20:21:15 +0900

    complete_game_code

--- first_game_code.c
+++ first_game_code.c
@@ -79,3 +79,255 @@ void drawBoard()
 
     refresh();
 }
+
+// 돌 뒤집기 + 놓을 자리 없을 경우 다음 턴으로 넘기기
+void flipPieces(int row, int col, int dRow, int dCol)
+{
+    CellState currentColor = (currentPlayer == PLAYER_BLACK) ? BLACK : WHITE;
+    CellState opponentColor = (currentPlayer == PLAYER_BLACK) ? WHITE : BLACK;
+
+    int newRow = row + dRow;
+    int newCol = col + dCol;
+
+    while (newRow >= 0 && newRow < BOARD_SIZE && newCol >= 0 && newCol < BOARD_SIZE)
+    {
+        if (board[newRow][newCol] == opponentColor)
+        {
+            newRow += dRow;
+            newCol += dCol;
+        }
+        else if (board[newRow][newCol] == currentColor)
+        {
+            while (newRow != row || newCol != col)
+            {
+                newRow -= dRow;
+                newCol -= dCol;
+                board[newRow][newCol] = currentColor;
+            }
+            return;
+        }
+        else if (board[newRow][newCol] == EMPTY)
+        {
+            return;
+        }
+    }
+}
+
+// 돌을 놓을 수 있는지 확인하는 함수
+int isMoveValid(int row, int col)
+{
+    if (board[row][col] != EMPTY)
+    {
+        return 0; // 이미 돌이 있는 위치이므로 유효하지 않음
+    }
+
+    CellState playerColor = (currentPlayer == PLAYER_BLACK) ? BLACK : WHITE;
+    CellState opponentColor = (currentPlayer == PLAYER_BLACK) ? WHITE : BLACK;
+
+    // 주변 8방향에 상대방 돌이 있는지 확인
+    for (int dRow = -1; dRow <= 1; dRow++)
+    {
+        for (int dCol = -1; dCol <= 1; dCol++)
+        {
+            if (dRow == 0 && dCol == 0)
+            {
+                continue;
+            }
+
+            int newRow = row + dRow;
+            int newCol = col + dCol;
+            int foundOpponentPiece = 0;
+
+            while (newRow >= 0 && newRow < BOARD_SIZE && newCol >= 0 && newCol < BOARD_SIZE)
+            {
+                if (board[newRow][newCol] == opponentColor)
+                {
+                    foundOpponentPiece = 1;
+                    newRow += dRow;
+                    newCol += dCol;
+                }
+                else if (board[newRow][newCol] == playerColor)
+                {
+                    if (foundOpponentPiece)
+                    {
+                        return 1; // 주변에 상대방 돌이 있고, 돌을 뒤집을 돌도 있으므로 유효함
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else if (board[newRow][newCol] == EMPTY)
+                {
+                    break; // 빈 공간이므로 유효하지 않음
+                }
+            }
+        }
+    }
+
+    return 0; // 주변에 상대방 돌이 없어 뒤집을 돌이 없으므로 유효하지 않음
+}
+
+// 돌 놓기
+void placePiece(int row, int col)
+{
+
+    if (!isMoveValid(row, col))
+    {
+        return; // 돌을 놓을 수 없는 경우 함수 종료
+    }
+
+    CellState playerColor = (currentPlayer == PLAYER_BLACK) ? BLACK : WHITE;
+    board[row][col] = playerColor;
+
+    for (int dRow = -1; dRow <= 1; dRow++)
+    {
+        for (int dCol = -1; dCol <= 1; dCol++)
+        {
+            if (dRow == 0 && dCol == 0)
+            {
+                continue;
+            }
+
+            flipPieces(row, col, dRow, dCol);
+        }
+    }
+
+    currentPlayer = (currentPlayer == PLAYER_BLACK) ? PLAYER_WHITE : PLAYER_BLACK;
+
+    // 상대방 플레이어가 돌을 놓을 수 있는지 확인
+    int opponentHasMove = 0;
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            if (isMoveValid(i, j))
+            {
+                opponentHasMove = 1;
+                break;
+            }
+        }
+        if (opponentHasMove)
+        {
+            break;
+        }
+    }
+
+    // 상대방 플레이어에게 턴을 넘김
+    if (!opponentHasMove)
+    {
+        currentPlayer = (currentPlayer == PLAYER_BLACK) ? PLAYER_WHITE : PLAYER_BLACK;
+    }
+}
+
+// 게임 종료 여부 체크
+int isGameOver()
+{
+    int blackCount = 0;
+    int whiteCount = 0;
+    int emptyCount = 0;
+
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            if (board[i][j] == BLACK)
+            {
+                blackCount++;
+            }
+            else if (board[i][j] == WHITE)
+            {
+                whiteCount++;
+            }
+            else if (board[i][j] == EMPTY)
+            {
+                emptyCount++;
+            }
+        }
+    }
+
+    if (blackCount == 0 || whiteCount == 0 || emptyCount == 0)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+// 게임 종료 후 결과 출력
+void printGameResult()
+{
+    int blackCount = 0;
+    int whiteCount = 0;
+
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            if (board[i][j] == BLACK)
+            {
+                blackCount++;
+            }
+            else if (board[i][j] == WHITE)
+            {
+                whiteCount++;
+            }
+        }
+    }
+
+    if (blackCount > whiteCount)
+    {
+        printw("Black wins!\n");
+    }
+    else if (blackCount < whiteCount)
+    {
+        printw("White wins!\n");
+    }
+    else
+    {
+        printw("It's a tie!\n");
+    }
+}
+
+// 메인 함수
+int main()
+{
+    initscr();
+    // cbreak();
+
+    keypad(stdscr, TRUE);
+    if (has_colors() == FALSE)
+    {
+        endwin();
+        printf("Your terminal does not support color\n");
+        return 1;
+    }
+    start_color();
+
+    initializeBoard();
+
+    while (!isGameOver())
+    {
+        drawBoard();
+
+        printw("Current Player: %s\n", (currentPlayer == PLAYER_BLACK) ? "BLACK" : "WHITE");
+        printw("Enter row and column (e.g., 1 2): ");
+
+        int row, col;
+        scanw("%d %d", &row, &col);
+
+        if (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE)
+        {
+            placePiece(row, col);
+        }
+    }
+
+    drawBoard();
+
+    printGameResult();
+
+    getch();
+    endwin();
+
+    return 0;
+}
